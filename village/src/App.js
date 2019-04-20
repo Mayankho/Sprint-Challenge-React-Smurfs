91108ac71b9c740da8e01ec1a8bd119b8ede2add
Mayankho <yankhot96@gmail.com>
2019-04-20 12:48:43 -0400

    Smurfs on state and updated form to include post

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router,Link,Route, NavLink} from 'react-router-dom';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -9,11 +11,18 @@ class App extends Component {
     super(props);
     this.state = {
       smurfs: [],
+      name: '',
+      age: '',
+      height: '',
     };
   }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount(){
+    axios 
+      .get('http://localhost:3333/smurfs')
+      .then(res => this.setState({smurfs: res.data}))
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
@@ -25,3 +34,9 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
+  // Notice what your map function is looping over and returning inside of Smurfs.
+  // You'll need to make sure you have the right properties on state and pass them down to props.
