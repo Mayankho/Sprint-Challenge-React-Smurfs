b169acebfd6394afcefff90bc4d03dada3ae4993
Mayankho <yankhot96@gmail.com>
2019-04-20 12:51:27 -0400

    Updated form button to have onclick function

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -58,7 +58,7 @@ class SmurfForm extends Component {
             value={this.state.height}
             name="height"
           />
-          <button type="submit">Add to the village</button>
+          <button onClick = {this.addSmurf} type="submit">Add to the village</button>
         </form>
       </div>
     );
